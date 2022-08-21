f3e048c61aaac58e877ef9c1699b5120cbe365f8
LopeKai <kaiqueoakley35@hotmail.com>
2022-08-21 12:27:38 -0300

    User control finished

---
+++ src/pages/Users/index.tsx
@@ -0,0 +1,80 @@
+import { Flex, SimpleGrid, Box, Text, theme, Heading, Button, Icon, Table, Thead, Tr, Th, Td, Checkbox, Tbody } from '@chakra-ui/react'
+import { RiAddLine, RiPencilLine } from 'react-icons/ri'
+
+import { Header } from '../../components/Header'
+import { Pagination } from '../../components/Pagination/pagination'
+import { SideBar } from '../../components/Sidebar/Sideber'
+
+export default function UserList() {
+    return (
+        <Box>
+            <Header />
+
+            <Flex w="100%" maxWidth={1480} my="6" px="6">
+                <SideBar />
+
+                <Box flex="1" borderRadius={8} bg="gray.800" p='8'>
+                    <Flex mb="8" justify="space-between" align="center">
+                        <Heading size="lg" fontWeight="normal">Usuários</Heading>
+
+                        <Button
+                            as="a"
+                            size="sm"
+                            fontSize="sm"
+                            colorScheme="pink"
+                            leftIcon={<Icon
+                                as={RiAddLine} 
+                                fontSize="20"
+                            />}
+                        >
+                            Criar novo
+                        </Button>
+                    </Flex>
+
+                    <Table colorScheme="whiteAlpha">
+                        <Thead>
+                            <Tr>
+                                <Th px="6" color="gray.300" width="8">
+                                    <Checkbox colorScheme="pink" />
+                                </Th>
+                                <Th>Usuários</Th>
+                                <Th>Data de cadastro</Th>
+                                <Th width="8"></Th>
+                            </Tr>
+                        </Thead>
+                        <Tbody>
+                            <Tr>
+                                <Td px="6">
+                                    <Checkbox colorScheme="pink" />
+                                </Td>
+                                <Td>
+                                    <Box>
+                                        <Text fontWeight="bold">Kaique Lopes</Text>
+                                        <Text fontSize="sm" color="gray.300">kaiquearaujo246@hotmail.com</Text>
+                                    </Box>
+                                </Td>
+                                <Td>04 de abril, 2022</Td>
+                                <Td>
+                                    <Button
+                                        as="a"
+                                        size="sm"
+                                        fontSize="sm"
+                                        colorScheme="purple"
+                                        leftIcon={<Icon
+                                            as={RiPencilLine} 
+                                            fontSize="16"
+                                        />}
+                                    >
+                                        Editar
+                                    </Button>
+                                </Td>
+                            </Tr>
+                        </Tbody>
+                    </Table>
+
+                    <Pagination />
+                </Box>
+            </Flex>
+        </Box>
+    )
+}
