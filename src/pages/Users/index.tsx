06221e2a3aec7086ee9aa2d4c2a8c2a37f09911a
LopeKai <kaiqueoakley35@hotmail.com>
2022-08-30 19:24:23 -0300

    adding axios and creating a hooks users

--- src/pages/Users/index.tsx
+++ src/pages/Users/index.tsx
@@ -1,36 +1,14 @@
 import { useBreakpointValue, Flex, SimpleGrid, Box, Text, theme, Heading, Button, Icon, Table, Thead, Tr, Th, Td, Checkbox, Tbody, Spinner } from '@chakra-ui/react'
 import Link from 'next/link'
-import { useEffect } from 'react'
 import { RiAddLine, RiPencilLine } from 'react-icons/ri'
-import { useQuery } from 'react-query'
-
 
 import { Header } from '../../components/Header'
 import { Pagination } from '../../components/Pagination'
 import { SideBar } from '../../components/Sidebar'
+import { useUsers } from '../../services/hooks/userUsers'
 
 export default function UserList() {
-
-    const { data, isLoading, error } = useQuery('users', async () => {
-        const response = await fetch("http://localhost:3000/api/users")
-        const data = await response.json()
-
-        const users = data.users.map(user => {
-            return {
-                id: user.id,
-                name: user.name,
-                email: user.email,
-                createdAt: new Date(user.createdAt).toLocaleDateString('pt-br',{
-                    day: '2-digit',
-                    month: 'long',
-                    year: 'numeric'
-                })
-            }
-        })
-        return users
-    }, {
-        staleTime: 1000 * 5, // 5 seconds nao vai precisar sem recarrgada
-    })
+    const { data, isLoading, isFetching, error } = useUsers()
 
     const isWideVersion = useBreakpointValue({
         base: false,
@@ -46,7 +24,10 @@ export default function UserList() {
 
                 <Box flex="1" borderRadius={8} bg="gray.800" p='8'>
                     <Flex mb="8" justify="space-between" align="center">
-                        <Heading size="lg" fontWeight="normal">Usuários</Heading>
+                        <Heading size="lg" fontWeight="normal">
+                            Usuários
+                            {!isLoading && isFetching && <Spinner size="sm" color="gray.500" ml="4" />}
+                        </Heading>
 
                         <Link href="/Users/create" passHref>
                             <Button
@@ -71,7 +52,7 @@ export default function UserList() {
                             </Flex>
                         ) : error ? (
                             <Flex justify="center">
-                                <Text>Fala ao obter dados dos usuários.</Text>
+                                <Text>Falha ao obter dados dos usuários.</Text>
                             </Flex>
                         ) : (
                             <>
